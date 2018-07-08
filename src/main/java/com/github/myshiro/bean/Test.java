c68b8ae49bc14f103738bcfa9737f790aebdc66a
raosiwen <siwen.rao@gmail.com>
2018-07-08 22:58:48 +0800

    测试提交

---
+++ src/main/java/com/github/myshiro/bean/Test.java
@@ -0,0 +1,15 @@
+package com.github.myshiro.bean;
+
+import java.util.Date;
+
+/**
+ * @author stefano
+ * @date ： 2018-06-10 12:28
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+    }
+
+}
